5a63cb358094ee8a27538a9a506546e1e30f36e6
FreeDropOracle <ahmedkhedhri99@gmail.com>
2026-04-28 11:14:30 +0000

    test(zk): replace generated proof dependency with committed CI fixture

--- packages/contracts/test/helpers/zkProof.ts
+++ packages/contracts/test/helpers/zkProof.ts
@@ -3,19 +3,8 @@ import path from "path";
 import { ethers } from "hardhat";
 
 export function loadDemoProofBytes(): Uint8Array {
-  const buildDir = path.join(__dirname, "../../../zk/build");
-  const proof = JSON.parse(fs.readFileSync(path.join(buildDir, "proof.json"), "utf-8"));
-  const publicSignals = JSON.parse(fs.readFileSync(path.join(buildDir, "public.json"), "utf-8"));
+  const fixturePath = path.join(__dirname, "./zkProofFixture.json");
+  const fixture = JSON.parse(fs.readFileSync(fixturePath, "utf-8"));
 
-  return ethers.getBytes(
-    ethers.AbiCoder.defaultAbiCoder().encode(
-      ["uint256[2]", "uint256[2][2]", "uint256[2]", "uint256[1]"],
-      [
-        [proof.pi_a[0], proof.pi_a[1]],
-        [[proof.pi_b[0][1], proof.pi_b[0][0]], [proof.pi_b[1][1], proof.pi_b[1][0]]],
-        [proof.pi_c[0], proof.pi_c[1]],
-        [publicSignals[0]]
-      ]
-    )
-  );
+  return ethers.getBytes(fixture.proofBytes);
 }
